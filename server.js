528d90f34c8e17478ef9fc5753438e274c0f7935
Irelander <iiitttiii@naver.com>
2021-03-11 22:36:11 +0900

    main router api 추가

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', function(param, req, res){
+    res.write(200);
+    res.end('Github Guide');
+})
+
+
 app.listen(3000);
