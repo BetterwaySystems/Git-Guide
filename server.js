ea4d8f560930353e794434247d8260f2b2a1ea8e
Irelander <iiitttiii@naver.com>
2021-03-15 10:43:29 +0900

    유저 조회 API 추가 #26

--- server.js
+++ server.js
@@ -7,5 +7,17 @@ app.get('/', function(param, req, res){
     res.end('Github Guide');
 })
 
+app.get('/api/user', function(param, req, res){
+    
+    function authorization(){
+        return '인증 사용자'
+    }
+    authorization();
+
+    res.write(200);
+    res.end('OK');
+
+})
+
 
 app.listen(3000);
